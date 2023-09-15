db0ec5f655f01d45e1cf643b4fb25d499001347b
Boubakri Youssef <youssefboubakri061@gmail.com>
2023-09-15 16:43:49 +0100

    Second Version of Inventory

--- src/main/java/com/example/inventoryapp/product/ProductController.java
+++ src/main/java/com/example/inventoryapp/product/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -37,6 +38,23 @@ public class ProductController {
         List<Product> listProducts = productRepository.findAll();
         model.addAttribute("listProducts",listProducts);
         return "products";
+    }
+
+    @GetMapping("products/edit/{id}")
+    public String showEditProductForm(@PathVariable("id") Integer id, Model model){
+        Product product = productRepository.findById(id).get();
+        model.addAttribute("product",product);
+        List<Category> listCategories = categoryRepository.findAll();
+        model.addAttribute("listCategories", listCategories);
+        return "product_form";
+    }
+
+    @GetMapping("products/delete/{id}")
+    public String deleteProduct(@PathVariable("id") Integer id, Model model){
+        productRepository.deleteById(id);
+        return "redirect:/products";
 
     }
+
+
 }
